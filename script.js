775ef15436f79fa963d1ce74c17f83f525f35011
Hamas2000 <168201374+Hamas2000@users.noreply.github.com>
2024-07-10 11:14:07 +0430

    linter error sloved

--- script.js
+++ script.js
@@ -18,9 +18,16 @@ let cid = [
 
 document.getElementById('price').innerHTML = `<b>Price:</b> ${price}`;
 
+const displayCashInDrawer = () => {
+  displayCid.innerHTML = `<h4>Cash in Drawer:</h4>${cid
+    .map((cash) => `${cash[0]}: $${cash[1].toFixed(2)} <br>`)
+    .reverse()
+    .join('')}`;
+};
+
 const checkRegister = () => {
   const cashInt = parseFloat(cash.value);
-  const change = Number((cashInt - price).toFixed(2));
+  let change = Number((cashInt - price).toFixed(2));
   const totalCid = Number(cid.reduce((total, sum) => total + sum[1], 0).toFixed(2));
 
   document.getElementById('change').innerHTML = `<b>Change:</b> ${change}`;
@@ -28,10 +35,14 @@ const checkRegister = () => {
   if (cashInt < price) {
     displayChangeDue.innerText = 'Customer does not have enough money to purchase the item';
     return;
-  } else if (cashInt === price) {
+  }
+  
+  if (cashInt === price) {
     displayChangeDue.innerText = 'No change due - customer paid with exact cash';
     return;
-  } else if (cash.value === '') {
+  }
+  
+  if (cash.value === '') {
     return;
   }
 
@@ -41,7 +52,10 @@ const checkRegister = () => {
   }
 
   const denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
-  const denominationNames = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 'QUARTER', 'DIME', 'NICKEL', 'PENNY'];
+  const denominationNames = [
+    'ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 
+    'QUARTER', 'DIME', 'NICKEL', 'PENNY'
+  ];
   const changeArr = [];
   const cidCopy = [...cid];
 
@@ -65,25 +79,25 @@ const checkRegister = () => {
 
   const remainingCid = cidCopy.reduce((total, sum) => total + sum[1], 0);
   if (remainingCid === 0) {
-    displayChangeDue.innerHTML = `Status: CLOSED ${changeArr.map(cash => `${cash[0]}: $${cash[1].toFixed(2)}`).join(' ')}`;
-    cid = cid.map(denom => [denom[0], 0]);
+    displayChangeDue.innerHTML = `Status: CLOSED ${changeArr
+      .map((cash) => `${cash[0]}: $${cash[1].toFixed(2)}`)
+      .join(' ')}`;
+    cid = cid.map((denom) => [denom[0], 0]);
   } else {
-    displayChangeDue.innerHTML = `Status: <b>OPEN</b> <br><br>${changeArr.map(cash => `<b>${cash[0]}</b>: $${cash[1].toFixed(2)} <br>`).join(' ')}`;
+    displayChangeDue.innerHTML = `Status: <b>OPEN</b> <br><br>${changeArr
+      .map((cash) => `<b>${cash[0]}</b>: $${cash[1].toFixed(2)} <br>`)
+      .join(' ')}`;
     cid = cidCopy;
   }
 
   displayCashInDrawer();
 };
 
-const displayCashInDrawer = () => {
-  displayCid.innerHTML = `<h4>Cash in Drawer:</h4>${cid.map(cash => `${cash[0]}: $${cash[1].toFixed(2)} <br>`).reverse().join('')}`;
-};
-
 window.onload = displayCashInDrawer;
 
 purchaseBtn.addEventListener('click', checkRegister);
 
-cash.addEventListener('keydown', e => {
+cash.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     checkRegister();
   }
